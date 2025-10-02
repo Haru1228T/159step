634c05ce816d346c658804d08f547ed7744cf48f
Haru1228T <haru1228tech@gmail.com>
2025-10-02 22:50:05 +0900

    /へアクセスした際にレスポンスを返す関数をhelloからraretech_messageに変更

--- app.py
+++ app.py
@@ -5,8 +5,9 @@ from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/', methods=['GET'])
-def hello():
-    return 'Hello, World'
+def raretech_message():
+    return '夢は、目標に向かって毎日歩みを進めた者だけが叶えられる。\
+            今日の二時間は、その一歩だ'
 
 @app.route('/time', methods=['GET'])
 def current_time():
